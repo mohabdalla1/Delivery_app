af8c3446e1761b33968e83f98d939e3b252415dc
mohabdalla1 <100388248+mohabdalla1@users.noreply.github.com>
2026-09-18 13:42:29 +0000

    Fix import statement typo

--- lib/features/admin/presentation/pages/customers_page.dart
+++ lib/features/admin/presentation/pages/customers_page.dart
@@ -1,4 +1,5 @@
-import 'package0:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class CustomersPage extends StatelessWidget {
   const CustomersPage({super.key});
