a5efc65733abb82385e471df1b0e55b0fef025ea
mohabdalla1 <100388248+mohabdalla1@users.noreply.github.com>
2026-09-24 11:37:40 +0200

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,34 +1,36 @@
 pluginManagement {
-        val flutterSdkPath = run {
-                    val properties = java.util.Properties()
-                            file("local.properties").inputStream().use { properties.load(it) }
-                                    val flutterSdkPath = properties.getProperty("flutter.sdk")
-                                            assert(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-                                                    flutterSdkPath
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        val localPropertiesFile = file("android/local.properties")
+        if (localPropertiesFile.exists()) {
+            localPropertiesFile.inputStream().use { properties.load(it) }
         }
+        properties.getProperty("flutter.sdk") ?: System.getenv("FLUTTER_ROOT")
+    }
 
-            includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+    assert(flutterSdkPath != null) { "flutter.sdk not set in local.properties or FLUTTER_ROOT not set" }
 
-                repositories {
-                            google()
-                                    mavenCentral()
-                                            gradlePluginPortal()
-                }
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
 
 plugins {
-        id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-            // قم بتثبيت النسخة 8.5.0 هنا مطابقة لملف build.gradle.kts لكي ينتهي التضارب نهائياً
-                id("com.android.application") version "8.5.0" apply false
-                    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
 dependencyResolutionManagement {
-        repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-            repositories {
-                        google()
-                                mavenCentral()
-            }
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 include(":app")
