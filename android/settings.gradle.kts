546c73eaf77e1e0f9e964dfd2559dd919e4fc7f7
mohabdalla1 <100388248+mohabdalla1@users.noreply.github.com>
2026-09-24 12:21:13 +0200

    Create settings.gradle.kts

---
+++ android/settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        val localPropertiesFile = file("local.properties")
+        if (localPropertiesFile.exists()) {
+            localPropertiesFile.inputStream().use { properties.load(it) }
+        }
+        properties.getProperty("flutter.sdk") ?: System.getenv("FLUTTER_ROOT")
+    }
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+}
+
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
+
+include(":app")
